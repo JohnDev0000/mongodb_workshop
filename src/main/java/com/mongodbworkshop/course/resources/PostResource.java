c16aef3f6bb262872cb286dc3c3f42dadb77ee22
JohnDev0000 <johncroft2236@hotmail.com>
2024-02-19 01:25:42 -0300

    Consulta simples com query methods

--- src/main/java/com/mongodbworkshop/course/resources/PostResource.java
+++ src/main/java/com/mongodbworkshop/course/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.mongodbworkshop.course.resources;
 import com.mongodbworkshop.course.domain.Post;
 import com.mongodbworkshop.course.domain.User;
 import com.mongodbworkshop.course.dto.UserDTO;
+import com.mongodbworkshop.course.resources.util.URL;
 import com.mongodbworkshop.course.services.PostService;
 import com.mongodbworkshop.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,14 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text",
+            defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
